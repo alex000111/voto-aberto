bc5241e599048b5f52831f2035281e0fa2d307ce
Igor <igor@votoaberto.org>
2026-09-19 19:07:51 -0300

    Fix CandidatePhoto fallback and use real TSE images

--- src/components/CandidatePhoto.tsx
+++ src/components/CandidatePhoto.tsx
@@ -15,21 +15,21 @@ interface Props {
  * Se não carregar, exibe um avatar com a inicial do nome.
  */
 const KNOWN_PHOTOS: Record<string, string> = {
-  'Luiz Inácio Lula da Silva': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Luiz_In%C3%A1cio_Lula_da_Silva_em_2022.jpg/400px-Luiz_In%C3%A1cio_Lula_da_Silva_em_2022.jpg',
-  'Tarcísio de Freitas': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Tarc%C3%ADsio_de_Freitas_em_2022.jpg/400px-Tarc%C3%ADsio_de_Freitas_em_2022.jpg',
-  'Ronaldo Caiado': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Ronaldo_Caiado.jpg/400px-Ronaldo_Caiado.jpg',
-  'Ciro Gomes': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Ciro_Gomes_2022_%28cropped%29.jpg/400px-Ciro_Gomes_2022_%28cropped%29.jpg',
-  'Guilherme Boulos': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Guilherme_Boulos_na_C%C3%A2mara_dos_Deputados_%28cropped%29.jpg/400px-Guilherme_Boulos_na_C%C3%A2mara_dos_Deputados_%28cropped%29.jpg',
-  'Tabata Amaral': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Tabata_Amaral_em_2023.jpg/400px-Tabata_Amaral_em_2023.jpg',
-  'Nikolas Ferreira': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Nikolas_Ferreira_em_2023.jpg/400px-Nikolas_Ferreira_em_2023.jpg',
-  'Gleisi Hoffmann': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Gleisi_Hoffmann_em_2023_%28cropped%29.jpg/400px-Gleisi_Hoffmann_em_2023_%28cropped%29.jpg',
-  'Pedro Campos': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Deputado_Pedro_Campos_em_2023_%28cropped%29.jpg/400px-Deputado_Pedro_Campos_em_2023_%28cropped%29.jpg',
-  'Jones Manoel': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Jones_Manoel_%28cropped%29.jpg/400px-Jones_Manoel_%28cropped%29.jpg',
-  'Eduardo Bolsonaro': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Eduardo_Bolsonaro_em_2019.jpg/400px-Eduardo_Bolsonaro_em_2019.jpg',
-  'João Campos': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Jo%C3%A3o_Campos_em_2023_%28cropped%29.jpg/400px-Jo%C3%A3o_Campos_em_2023_%28cropped%29.jpg',
-  'Raquel Lyra': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Raquel_Lyra_em_2023_%28cropped%29.jpg/400px-Raquel_Lyra_em_2023_%28cropped%29.jpg',
-  'Marília Arraes': 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Mar%C3%ADlia_Arraes_2022.jpg/400px-Mar%C3%ADlia_Arraes_2022.jpg',
+  'Luiz Inácio Lula da Silva': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/280001607829/BR',
+  'Tarcísio de Freitas': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/250001612497/SP',
+  'Guilherme Boulos': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/250001614742/SP',
+  'Tabata Amaral': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/250001613271/SP',
+  'Nikolas Ferreira': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/130001616492/MG',
+  'Gleisi Hoffmann': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/160001606555/PR',
+  'Pedro Campos': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/170001619899/PE',
+  'Jones Manoel': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/170001607787/PE',
+  'André Ferreira': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/170001605333/PE',
+  'Raquel Lyra': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/170001607831/PE',
+  'Marília Arraes': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/170001625902/PE',
+  'Eduardo Bolsonaro': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2040602022/250001613346/SP',
+  'João Campos': 'https://divulgacandcontas.tse.jus.br/divulga/rest/arquivo/img/2030402020/170000780283/PE',
 };
+
 
 export default function CandidatePhoto({ sourceId, uf, name, size = 64 }: Props) {
   const [failed, setFailed] = useState(false);
@@ -46,31 +46,8 @@ export default function CandidatePhoto({ sourceId, uf, name, size = 64 }: Props)
 
   const initial = (name || '?').charAt(0).toUpperCase();
 
-  if (photoUrl) {
-    return (
-      // eslint-disable-next-line @next/next/no-img-element
-      <img
-        src={photoUrl}
-        alt={`Foto oficial de ${name}`}
-        width={size}
-        height={size}
-        onError={() => setFailed(true)}
-        style={{
-          width: size,
-          height: size,
-          borderRadius: '50%',
-          objectFit: 'cover',
-          objectPosition: 'top',
-          border: '2px solid var(--line)',
-          background: 'var(--surface-subtle)',
-          flexShrink: 0,
-        }}
-      />
-    );
-  }
-
-  // Avatar fallback com inicial
-  return (
+  // Fallback com a inicial
+  const fallback = (
     <div
       aria-label={`Foto não disponível para ${name}`}
       style={{
@@ -90,6 +67,35 @@ export default function CandidatePhoto({ sourceId, uf, name, size = 64 }: Props)
       }}
     >
       {initial}
+    </div>
+  );
+
+  return (
+    <div style={{ position: 'relative', width: size, height: size, flexShrink: 0 }}>
+      {(!photoUrl || failed) && fallback}
+      {photoUrl && (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={photoUrl}
+          alt={`Foto oficial de ${name}`}
+          width={size}
+          height={size}
+          onError={() => setFailed(true)}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: size,
+            height: size,
+            borderRadius: '50%',
+            objectFit: 'cover',
+            objectPosition: 'top',
+            border: '2px solid var(--line)',
+            background: 'var(--surface-subtle)',
+            opacity: failed ? 0 : 1, // Esconde a imagem se falhar
+          }}
+        />
+      )}
     </div>
   );
 }
